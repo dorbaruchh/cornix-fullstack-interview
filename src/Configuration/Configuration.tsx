5b38a7cf5f8ce6948bdb01185097fec138667109
Dor Baruch <dor.baruch@gmail.com>
2022-01-27 08:45:58 +0200

    Updated project

---
+++ src/Configuration/Configuration.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Configuration = () => {
+  return <Wrapper>Add code here</Wrapper>;
+};
+
+export default Configuration;
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
---
+++ template_files/index.d1d6f176.js
@@ -0,0 +1,1 @@
+function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","UA-113877696-1");
